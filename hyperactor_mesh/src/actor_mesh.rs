14ccb23e40ad35391ffeb4fb9809aa3fac649351
Shayne Fletcher <shaynefletcher@fb.com>
2025-06-16 09:11:59 -0700

    [hyperactor_mesh]: actor_mesh: test: test_pingpong_full_mesh
    Pull Request resolved: https://github.com/pytorch-labs/monarch/pull/274
    
    
    add test to verify 6-neighbor ping-pong messaging across full 3×3×3 actor mesh.
    ghstack-source-id: 290705835
    
    Differential Revision: [D76680489](https://our.internmc.facebook.com/intern/diff/D76680489/)
    
    **NOTE FOR REVIEWERS**: This PR has internal Meta-specific changes or comments, please review them on [Phabricator](https://our.internmc.facebook.com/intern/diff/D76680489/)!

--- hyperactor_mesh/src/actor_mesh.rs
+++ hyperactor_mesh/src/actor_mesh.rs
@@ -623,6 +623,62 @@ mod tests {
             }
 
             #[tokio::test]
+            async fn test_pingpong_full_mesh() {
+                use hyperactor::test_utils::pingpong::PingPongActor;
+                use hyperactor::test_utils::pingpong::PingPongActorParams;
+                use hyperactor::test_utils::pingpong::PingPongMessage;
+
+                use futures::future::join_all;
+
+                const X: usize = 3;
+                const Y: usize = 3;
+                const Z: usize = 3;
+                let alloc = $allocator
+                    .allocate(AllocSpec {
+                        shape: shape! { x = X, y = Y, z = Z },
+                        constraints: Default::default(),
+                    })
+                    .await
+                    .unwrap();
+
+                let proc_mesh = ProcMesh::allocate(alloc).await.unwrap();
+                let (undeliverable_tx, _undeliverable_rx) = proc_mesh.client().open_port();
+                let params = PingPongActorParams::new(undeliverable_tx.bind(), None);
+                let actor_mesh: RootActorMesh<PingPongActor> =
+                    proc_mesh.spawn::<PingPongActor>("pingpong", &params).await.unwrap();
+                let shape = actor_mesh.shape();
+                let slice = shape.slice();
+
+                // Collect futures for all OncePorts.
+                let mut futures = Vec::new();
+
+                for rank in slice.iter() {
+                    let actor = actor_mesh.get(rank).unwrap();
+                    let coords = (&slice.coordinates(rank).unwrap()[..]).try_into().unwrap();
+                    let sizes = (&slice.sizes())[..].try_into().unwrap();
+                    let neighbors = ndslice::utils::stencil::von_neumann_neighbors::<3>();
+                    for neighbor_coords in ndslice::utils::apply_stencil(&coords, sizes, &neighbors) {
+                        if let Ok(neighbor_rank) = shape.slice().location(&neighbor_coords) {
+                            let neighbor = actor_mesh.get(neighbor_rank).unwrap();
+                            let (done_tx, done_rx) = proc_mesh.client().open_once_port();
+                            actor
+                                .send(
+                                    proc_mesh.client(),
+                                    PingPongMessage(4, neighbor.clone(), done_tx.bind()),
+                                )
+                                .unwrap();
+                            futures.push(done_rx.recv());
+                        }
+                    }
+                }
+
+                let results = join_all(futures).await;
+                for result in results {
+                    assert_eq!(result.unwrap(), true);
+                }
+            }
+
+            #[tokio::test]
             async fn test_cast() {
                 let alloc = $allocator
                     .allocate(AllocSpec {
